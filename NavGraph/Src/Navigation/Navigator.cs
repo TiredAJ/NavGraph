96955b128d38038dbd82ad71745f101195e26fbb
AJ <aletha.e.jones@gmail.com>
2024-02-03 01:16:12 +0000

    Navigator disappeared, but it's fine, wanted to restart anyways. Got a new blank canvas now and I think I know where it's going?

---
+++ NavGraph/Src/Navigation/Navigator.cs
@@ -0,0 +1,26 @@
+﻿// Ignore Spelling: Nav Dest
+
+namespace NavGraphTools.Navigation;
+
+public class Navigator
+{
+    Queue<UserDirections> Path_Dir = new Queue<UserDirections>();
+    Stack<int> Path_Nodes = new Stack<int>();
+    Stack<int> Pre_PathNodes = new Stack<int>();
+
+    public void StartNavigation()
+    {
+    }
+}
+
+public enum UserDirections
+{
+    TurnLeft,       //turn left in a corridor
+    TurnRight,      //turn right in a corridor
+    GoForward,      //just go forward
+    TurnLeftJunc,   //at a junction, turn left
+    TurnRightJunc,  //at a junction, turn right
+    GoStraightJunc, //at a junction, go forward
+    GoUpFloor,      //go up stairs/elevator
+    GoDownFloor     //go down stairs/elevator
+}
---
+++ NavGraph/Src/Navigation/Pather.cs
@@ -0,0 +1,112 @@
+﻿using System.Diagnostics;
+
+namespace NavGraphTools.Navigation;
+
+public class Pather
+{
+    #region Variables
+    private NavNode? Origin { get; set; } = null;
+    private NavNode? Destination { get; set; } = null;
+    private NavNode? CurrentLocation = null;
+    private ReadonlyNavGraph NG;
+    private bool Initialised = false;
+
+    //states
+    //private bool IsSFloor = false;
+    //private bool IsSBlock = false;
+    //private bool IsSGFloor = false;
+    #endregion
+
+    public Pather
+        (ref ReadonlyNavGraph _NG, NavNode _D, NavNode _O)
+    {
+        NG = NG;
+        Destination = _D;
+        Origin = _O;
+    }
+
+    public NavigatorErrors Start()
+    {
+        if (!Initialised)
+        { return NavigatorErrors.NotInitialised; }
+        else if (Origin == null || Destination == null)
+        { return NavigatorErrors.NullNodes; }
+
+        if (Origin.BlockName != Destination.BlockName)
+        { NavigateToGateway(Origin); }
+        else if (Origin.Floor != Destination.Floor)
+        { NavigateToElevation(Origin); }
+
+        throw new NotImplementedException();
+    }
+
+    private bool NavigateToGateway(NavNode _SubOrigin)
+    {
+        if (_SubOrigin is IGatewayFlow IGF && IGF.GatewayFlowDirection == null)
+        { /*check neighbours*/ }
+        else if (_SubOrigin is IGatewayFlow)
+        { SlideUntil(X => X is GatewayNode, _SubOrigin, false, 10000); }
+
+        throw new NotImplementedException();
+    }
+
+    private bool NavigateToElevation(NavNode _SubOrigin)
+    {
+        if (_SubOrigin is IElevationFlow IEF && IEF.ElvFlowDirection == null)
+        { /*check neighbours*/ }
+        else if (_SubOrigin is IElevationFlow)
+        { SlideUntil(X => X is GatewayNode, _SubOrigin, false, 10000); }
+
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Follows the flow until something
+    /// </summary>
+    /// <param name="Predicate">The 'something' that stops the flow</param>
+    /// <param name="_SubOrigin">The starting point</param>
+    /// <param name="_ElvFlow">Whether to follow Elevation flow (true) or Gateway flow (false)</param>
+    /// <param name="_MillisecondTimeout">NOT IMPLEMENTED - how long until it just gives up</param>
+    /// <returns></returns>
+    private List<int> SlideUntil
+        (Predicate<NavNode> Predicate, NavNode _SubOrigin,
+        bool _ElvFlow, long _MillisecondTimeout)
+    {
+        Stopwatch SW = new Stopwatch();
+        NavNode Position = _SubOrigin;
+        List<int> SubPath = new List<int>();
+        int PosUID = 0;
+        bool Reached = false;
+
+        while (SW.ElapsedMilliseconds < _MillisecondTimeout && !Reached)
+        {
+            if (_ElvFlow && Position is IElevationFlow IEF && IEF.ElvFlowDirection != null)
+            { PosUID = Position.GetNode((NodeDirection)IEF.ElvFlowDirection); }
+            else if (!_ElvFlow && Position is IGatewayFlow IGF && IGF.GatewayFlowDirection != null)
+            { PosUID = Position.GetNode((NodeDirection)IGF.GatewayFlowDirection); }
+
+            if (Predicate(NG.TryGetNode(PosUID)))
+            {
+                SubPath.Add(PosUID);
+                return SubPath;
+            }
+            else
+            {
+                Position = NG.TryGetNode(PosUID);
+
+                if (Position == null)
+                { throw new Exception("NodeWasNull?"); }
+            }
+        }
+
+        return SubPath;
+    }
+
+    public enum NavigatorErrors
+    {
+        None = 0,
+        NullNodes = 1,
+        NotInitialised = 2,
+
+    }
+}
